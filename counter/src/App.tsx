b7d32a5d7626802ad45d018e46dd6af659fbca06
Paulo Mário Valente Bumba <143729787+PauloBumba@users.noreply.github.com>
2024-07-03 21:20:12 -0300

    UPDATE

---
+++ counter/src/App.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import { Count } from './Components/Counter';
+
+
+const App: React.FC = () => {
+    return (
+        <div>
+            <h1>App Component</h1>
+            <Count/>
+        </div>
+    );
+};
+
+export default App;
---
+++ counter/src/Components/Counter.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+export const Count : React.FC = ()=>{
+    return (
+      <h1>Paulo</h1>  
+    )
+}
