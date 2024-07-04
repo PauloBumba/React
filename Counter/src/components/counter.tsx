8329de7b70c2a31d8dfc256199389e4f684245c6
Paulo Mário Valente Bumba <143729787+PauloBumba@users.noreply.github.com>
2024-07-04 19:41:57 -0300

    contador
    utilizado então os hook de estado para conntrolar o estado da aplicação

--- Counter/src/components/counter.tsx
+++ Counter/src/components/counter.tsx
@@ -1,13 +1,27 @@
-// src/components/Counter.tsx
-import React from 'react';
+import React, { useState } from 'react';
+import Styles from "../components/Styles.module.scss";
 
 const Counter: React.FC = () => {
-  return(
-    <div>
-        <button>aumentar</button>
-        <button>aumentar</button>
-    </div>
-  )
+  const [Contador, setContador] = useState(0);
+
+  return (
+    <main>
+     
+      <div className={Styles.player}>
+        <button onClick={() => {
+          setContador(Contador -1)
+        }}>
+          Diminuir
+        </button>
+        <button onClick={() => {
+          setContador(Contador + 1);
+        }}>
+          Aumentar
+        </button>
+        <h1>{Contador}</h1>
+      </div>
+    </main>
+  );
 };
 
 export default Counter;
